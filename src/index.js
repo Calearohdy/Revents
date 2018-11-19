45335184c7b42c55140efd6f3fb0e0ac34b70940
Cale Rohdy <calearohdy@gmail.com>
2018-11-19 12:15:22 -0500

    add routing

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(<App />, rootEl)
-}
+        ReactDOM.render( 
+        <BrowserRouter> 
+            <App/> 
+        </BrowserRouter>, rootEl)
+        }
 
-if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    })
-}
+        if (module.hot) {
+            module.hot.accept('./app/layout/App', () => {
+                setTimeout(render)
+            })
+        }
 
-render();
+        render();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        // If you want your app to work offline and load faster, you can change
+        // unregister() to register() below. Note this comes with some pitfalls.
+        // Learn more about service workers: http://bit.ly/CRA-PWA
+        serviceWorker.unregister();
