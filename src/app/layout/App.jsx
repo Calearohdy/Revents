ecdeba81b02c93cb3fd6f128697f650a9b162cb5
Cale Rohdy <calearohdy@gmail.com>
2018-11-19 15:13:52 -0500

    added redux

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,6 +9,7 @@ import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashbooar
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
 import HomePage from '../../features/home/HomePage';
+import TestComponent from '../../features/test/TestComponent';
 
 class App extends Component {
   render() {
@@ -25,6 +26,7 @@ class App extends Component {
             <Switch>
               <Route path='/events' component={EventDashboard} />
               <Route path='/event/:id' component={EventDetailedPage} />
+              <Route path='/test' component={TestComponent} />
               <Route path='/people' component={PeopleDashboard} />
               <Route path='/profile/:id' component={UserDetailedPage} />
               <Route path='/settings' component={SettingsDashboard} />
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './app/layout/App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from './app/store/configureStore'
 
 const rootEl = document.getElementById('root');
+const store = configureStore();
 
 let render = () => {
         ReactDOM.render( 
-        <BrowserRouter> 
-            <App/> 
-        </BrowserRouter>, rootEl)
+            <Provider store={store}>
+                <BrowserRouter> 
+                    <App/> 
+                </BrowserRouter>
+            </Provider>, rootEl)
         }
 
         if (module.hot) {
